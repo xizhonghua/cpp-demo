8b21ace70c097584c591098800871cdf66b6daa6
Zhonghua Xi <xizhonghua@gmail.com>
2015-10-11 15:00:45 -0400

    update

--- mesh_opt/simplication.cpp
+++ mesh_opt/simplication.cpp
@@ -1,6 +1,52 @@
-#include <iostream>
+#include <fstream>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
+// Simplification function
+#include <CGAL/Surface_mesh_simplification/edge_collapse.h>
+// Stop-condition policy
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Count_stop_predicate.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Edge_length_cost.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Midpoint_placement.h>
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef CGAL::Polyhedron_3<Kernel> Surface_mesh;
+namespace SMS = CGAL::Surface_mesh_simplification;
 
-int main() {
-  std::cout<<"hello";
-  return 0;
+int main(int argc, char** argv) {
+	Surface_mesh surface_mesh;
+
+	std::cout << "Reading '" << argv[1] << "' ..." << std::endl;
+
+	std::ifstream is(argv[1]);
+	is >> surface_mesh;
+	// This is a stop predicate (defines when the algorithm terminates).
+	// In this example, the simplification stops when the number of undirected edges
+	// left in the surface mesh drops below the specified number (1000)
+	SMS::Count_stop_predicate<Surface_mesh> stop(1000);
+
+	std::cout << "before v/e/f = " << surface_mesh.size_of_vertices() << "/"
+			<< surface_mesh.size_of_halfedges() / 2 << "/"
+			<< surface_mesh.size_of_facets() << std::endl;
+
+	// This the actual call to the simplification algorithm.
+	// The surface mesh and stop conditions are mandatory arguments.
+	// The index maps are needed because the vertices and edges
+	// of this surface mesh lack an "id()" field.
+	int r =
+			SMS::edge_collapse(surface_mesh, stop,
+					CGAL::vertex_index_map(
+							get(CGAL::vertex_external_index, surface_mesh)).halfedge_index_map(
+							get(CGAL::halfedge_external_index, surface_mesh)).get_cost(
+							SMS::Edge_length_cost<Surface_mesh>()).get_placement(
+							SMS::Midpoint_placement<Surface_mesh>()));
+
+	std::cout << "after v/e/f = " << surface_mesh.size_of_vertices() << "/"
+				<< surface_mesh.size_of_halfedges() / 2 << "/"
+				<< surface_mesh.size_of_facets() << std::endl;
+
+	std::ofstream os(argc > 2 ? argv[2] : "out.off");
+	os << surface_mesh;
+
+	return 0;
 }
